1e23a72a24c711ea9752cc49b38bf2299c7e8f9c
FrancoFlores1101 <103612128+FrancoFlores1101@users.noreply.github.com>
2022-04-18 11:17:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import clasemail
+
+
+def modificarcontrasenia():
+    print('ingrese la contraseña actual para modificar')
+    if(input() == email.contraseña):
+        print('ingrese la nueva contraseña')
+        email.contrasenia=input()
+    else:
+        print('contraseña incorrecta')
+
+def crearobjeto():
+    print('ingrese mail')
+    email2=clasemail.clase.CrearCuentainput()
+
+if __name__ == '__main__':
+    print('ingrese nombre y su id de cuenta,dominio,tipo de dominio y contraseña')
+    nom=input()
+    email=clasemail.clase(input(),input(),input(),input())
+    print('estimado',nom,'te enviaremos mensajes a la dirección',email.retornaEmail())
+    modificarcontrasenia()
+    crearobjeto()
+
+#Falta inciso 4, no supe realizarlo
